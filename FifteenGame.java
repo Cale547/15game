e54efd62c97fb1f74ba9493081cceb165e17880e
Cale547 <119942117+Cale547@users.noreply.github.com>
2024-04-24 16:19:03 +0200

    FifteenGame is no longer dependant on presenter and does its own thang. Also added some things to make PuzzleMenu work

--- FifteenGame.java
+++ FifteenGame.java
@@ -1,23 +1,77 @@
 import java.awt.BorderLayout;
+import java.awt.TextField;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowEvent;
 import java.io.File;
 
 import javax.swing.AbstractAction;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JRootPane;
 import javax.swing.KeyStroke;
 import javax.swing.JComponent;
 
-public class FifteenGame extends Presenter {
+public class FifteenGame {
+    JFrame frame;
+	TextField txt = new TextField(20);
+
     private BlockPanel pan;
     private PicComp winComp;
     private boolean gameOver = false;
+    private String folder;
+    private String extension;
+    private JFrame puzzlemenu;
 
-    public FifteenGame() {
-        createWinComp(pan.folder + "result" + pan.extension);
+    public FifteenGame(String f, String e, JFrame pm) {
+        puzzlemenu = pm;
+        folder = f;
+        extension = e;
+
+        frame = new JFrame();
+		frame.setBounds(0, 0, 418, 520);
+		// setup layout manager for frame
+		frame.setLayout(new BorderLayout());
+
+		JButton northButton = new JButton("North");
+		JButton eastButton = new JButton("East");
+		JButton southButton = new JButton("South");
+		JButton westButton = new JButton("West");
+
+		northButton.addActionListener(event->northButtonPressed());		
+		eastButton.addActionListener(event->eastButtonPressed());		
+		southButton.addActionListener(event->southButtonPressed());		
+		westButton.addActionListener(event->westButtonPressed());		
+
+		// add northButton to a JPanel, add that lpanel to frame
+		JPanel buttonPane = new JPanel();
+		buttonPane.setLayout(new BorderLayout());
+		buttonPane.add(northButton, BorderLayout.NORTH);
+		buttonPane.add(eastButton, BorderLayout.EAST);
+		buttonPane.add(southButton, BorderLayout.SOUTH);
+		buttonPane.add(westButton, BorderLayout.WEST);
+
+		// create other components (text component, e.g.), add them to frame
+		JPanel pane = new JPanel();
+		pane.setLayout(new BorderLayout());
+		
+		pane.add(buttonPane, BorderLayout.WEST);		
+		pane.add(txt, BorderLayout.EAST);
+		frame.add(pane, BorderLayout.SOUTH);
+
+		JComponent centerComponent = createCenterComponent();
+		// add centerComponent to frame
+		frame.add(centerComponent, BorderLayout.CENTER);
+		// show frame	
+		
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	Not needed when part of PuzzleMenu
+		frame.setVisible(true);
+
+        
+        createWinComp(f + "result" + e);
         showText("Press a button to start the game");
-
-        JRootPane rootPane = getFrame().getRootPane();
+        txt.setFocusable(false);
+        JRootPane rootPane = frame.getRootPane();
         rootPane.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("LEFT"), "doWest");
         rootPane.getActionMap().put("doWest",
         new AbstractAction() {
@@ -44,7 +98,7 @@ public class FifteenGame extends Presenter {
     }
 
     public BlockPanel createCenterComponent() {
-        pan = new BlockPanel();
+        pan = new BlockPanel(folder, extension);
         return pan;
     }
 
@@ -73,6 +127,13 @@ public class FifteenGame extends Presenter {
             showText(pan.getHole().n.i + " moved SOUTH");
             pan.switchSpot(pan.getHole(), pan.getHole().n);
             calculateWin(false);
+            return;
+        }
+
+        if (gameOver) {
+            gameOver = false;
+            this.frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+            puzzlemenu.setState(JFrame.NORMAL);
         }
 	}
 
@@ -86,11 +147,11 @@ public class FifteenGame extends Presenter {
 
         if (gameOver) {
             gameOver = false;
-            pan = new BlockPanel();
-            getFrame().remove(winComp);
-            getFrame().add(pan, BorderLayout.CENTER);
-            getFrame().setExtendedState(JFrame.MAXIMIZED_BOTH);
-            getFrame().setExtendedState(JFrame.NORMAL);
+            pan = new BlockPanel(folder, extension);
+            frame.remove(winComp);
+            frame.add(pan, BorderLayout.CENTER);
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            frame.setExtendedState(JFrame.NORMAL);
             showText("Press a button to start the game");
         }
 	}
@@ -105,18 +166,23 @@ public class FifteenGame extends Presenter {
         }
         if (win || autoWin) {
             gameOver = true;
-            showText("You win! Press 'North' to begin a new game.");
-            getFrame().remove(pan);
-            getFrame().add(winComp, BorderLayout.CENTER);
+            showText("You win! Press 'North' to begin a new game. Press 'South' to choose a new puzzle.");
+            frame.remove(pan);
+            frame.add(winComp, BorderLayout.CENTER);
             //rad 139-140 krävs för att framen ska uppdateras
-            getFrame().setExtendedState(JFrame.MAXIMIZED_BOTH);
-            getFrame().setExtendedState(JFrame.NORMAL);    
+            frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+            frame.setExtendedState(JFrame.NORMAL);    
             
         }
     }
 
+    public void showText(String text) {
+		// update the text component
+		txt.setText(text);
+	}
+
     public static void main(String[] args) {
-        new FifteenGame();
+        new FifteenGame("15ImagesNUMBERS/", ".jpg", new JFrame());
     }
 
 }
